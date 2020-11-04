2c53cb11888135975281d194429264381c97210e
Tomosuke421 <65202795+Tomosuke421@users.noreply.github.com>
2020-11-05 00:29:10 +0900

    section2-q2
    自力でできました。（泣）

--- section2-q1/sketch.js
+++ section2-q1/sketch.js
@@ -1,6 +1,10 @@
 // 小手調べ
 function setup(){
   for(let i = 0; i < 10; i++){
-    // BLANK[1]
+    noFill();
+    ellipse(50,50,i*10,i*10);
+if(i<5){stroke(0,0,255)}
+else{stroke(255,0,0)}
+
   }
 }
--- section2-q2/sketch.js
+++ section2-q2/sketch.js
@@ -6,6 +6,21 @@ function setup() {
   for(let i = 0; i < 8; i++){
     for(let j = 0; j < 8; j++){
       // BLANK[1] (hint: rectのx座標は size * i, y座標はsize * j)
-    }
-  }
+      if((i + j) % 2 ==1){
+        fill(128)
+      }
+      else{
+        fill(255)
+      }
+rect(size * i, size * j, size, size);
+if((i +　j) % 2 ==1 && j < 3){
+  fill(255,0,0)
+  ellipse((size * i + size *( i + 1)) / 2, (size * j + size * (j + 1)) / 2, 20)
+}
+if((i +　j) % 2 ==1 && j > 4){
+  fill(0,0,0)
+  ellipse((size * i + size *( i + 1)) / 2, (size * j + size * (j + 1)) / 2, 20)
+}
+}
+}
 }
