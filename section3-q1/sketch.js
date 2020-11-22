fff721768112daadabb34952a184bf0e5ff77194
Tomosuke421 <65202795+Tomosuke421@users.noreply.github.com>
2020-11-22 23:24:51 +0900

    3-q1

--- section3-q1/sketch.js
+++ section3-q1/sketch.js
@@ -4,15 +4,19 @@
 function setup(){
   createCanvas(400, 400);
   background(255);
-  balloon("I love keyakizaka46");
+  balloon("I love keyakizaka46",200,100,60);
 }
 
-function balloon(t){
+function balloon(t,c,x,y){
   let w = textWidth(t);
   let h = textAscent() + textDescent();
-  let p = 2;
-  fill(0);
+  let p = 8;
+  fill(c);
+  noStroke();
   rect(0, 0, w + p * 2, h + p * 2);
-  fill(255);
+  fill(0);
   text(t, p, h + p);
+  fill(c);
+  noStroke();
+  triangle(w+p*2-25,h+p*2,x,y,w+p*2-5,h+p*2);
 }
