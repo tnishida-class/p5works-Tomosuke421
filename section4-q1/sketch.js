23f00f973edd2fe331b75e5b1c309f0f3e2cda77
Tomosuke421 <65202795+Tomosuke421@users.noreply.github.com>
2020-11-23 14:46:33 +0900

    4-q1

--- section4-q1/sketch.js
+++ section4-q1/sketch.js
@@ -1,7 +1,7 @@
 // 練習問題「心臓の鼓動のように大きくなったり小さくなったりする円」
 let count;
 let cycle;
-
+let size;
 function setup(){
   createCanvas(200, 200);
   count = 0;
@@ -10,7 +10,14 @@ function setup(){
 
 function draw(){
   background(160, 192, 255);
-  count = (count + 1) % cycle;
-  // BLANK[1]
+    count = (count + 1) % cycle;
+  if(count<50){
+
+    size= count+50;
+  }
+  else{
+    size=150-count;
+  }
+  noStroke();
   ellipse(width / 2, height / 2, size);
 }
